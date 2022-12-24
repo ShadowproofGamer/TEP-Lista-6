dd1a1d840fdf7cb9f0b7143cbbf18d04834a7ff3
ShadowproofGamer <57487079+ShadowproofGamer@users.noreply.github.com>
2022-12-24 10:28:28 +0100

    Dodaj pliki projektów.

---
+++ TEP-Lista-6/CGeneticAlgorithm.cpp
@@ -0,0 +1,51 @@
+#include "CGeneticAlgorithm.h"
+
+
+CGeneticAlgorithm::CGeneticAlgorithm() {
+	backpackSize = DEFBACKPACKSIZE;
+	for (int i = 0; i < DEFITEMCOUNT; i++)
+	{
+		itemTable[i] = make_pair(i,i);
+	}
+	constructionError = 0;
+}
+CGeneticAlgorithm::CGeneticAlgorithm(double size) {
+	if (size > 0) backpackSize = size;
+	else
+	{
+		backpackSize = DEFBACKPACKSIZE;
+		constructionError = ERRORSIZE;
+	}
+	for (int i = 0; i < DEFITEMCOUNT; i++)
+	{
+		itemTable[i] = make_pair(i, i);
+	}
+
+}
+CGeneticAlgorithm::CGeneticAlgorithm(vector<pair<double, double>> table) {
+	if (table.size() > 0) itemTable = table;
+	else
+	{
+		for (int i = 0; i < DEFITEMCOUNT; i++)
+		{
+			itemTable[i] = make_pair(i, i);
+		}
+		constructionError = ERRORVECTOR;
+	}
+	backpackSize = DEFBACKPACKSIZE;
+}
+CGeneticAlgorithm::CGeneticAlgorithm(double size, vector<pair<double, double>> table) {
+	if (size > 0) backpackSize = size;
+	else constructionError = ERRORSIZE;
+
+	if (table.size() > 0) itemTable = table;
+	else 
+	{
+		for (int i = 0; i < DEFITEMCOUNT; i++)
+		{
+			itemTable[i] = make_pair(i, i);
+		}
+		if (constructionError == ERRORSIZE)constructionError = ERRORBOTH;
+		else constructionError = ERRORVECTOR;
+	}
+};
---
+++ TEP-Lista-6/constants.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <vector>
+#include <tuple>
+#define DEFITEMCOUNT 10
+#define DEFBACKPACKSIZE 10 
+#define ERRORSIZE -1
+#define ERRORVECTOR -2
+#define ERRORBOTH -3
